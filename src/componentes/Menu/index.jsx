fff2bf957db01f7fa68ea62097b31074de5dbc69
Alisson Jaques <alissonjaquesrmq@gmail.com>
2023-03-17 08:35:14 -0300

    Refatorando os ícones globais da aplicação

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,35 +1,19 @@
 import React from 'react';
-import home from '../../assets/icones/home.png';
-import maisCurtidas from '../../assets/icones/mais-curtidas.png';
-import maisVistas from '../../assets/icones/mais-vistas.png';
-import novas from '../../assets/icones/novas.png';
-import surpreendaMe from '../../assets/icones/surpreenda-me.png';
+import icones from './icones.json';
 import styles from './Menu.module.scss';
 
 export default function Menu() {
   return (
     <nav className={styles.menu}>
       <ul className={styles.menu__lista}>
-        <li className={styles.menu__item}>
-          <img src={home} alt="" />
-          <a href="/">Início</a>
-        </li>
-        <li className={styles.menu__item}>
-          <img src={maisCurtidas} alt="" />
-          <a href="/">Mais Curtidas</a>
-        </li>
-        <li className={styles.menu__item}>
-          <img src={maisVistas} alt="" />
-          <a href="/">Mais Vistas</a>
-        </li>
-        <li className={styles.menu__item}>
-          <img src={novas} alt="" />
-          <a href="/">Novas</a>
-        </li>
-        <li className={styles.menu__item}>
-          <img src={surpreendaMe} alt="" />
-          <a href="/">Surpreenda-me</a>
-        </li>
+        {icones.map((icone)=>{ 
+          return (
+            <li className={styles.menu__item}>
+              <img src={icone.imagem} alt={icone.descricao} />
+              <a href="/">{icone.titulo}</a>
+            </li>)
+          })
+        }
       </ul>
     </nav>
   );
